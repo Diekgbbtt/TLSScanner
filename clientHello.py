f2b805a3e7b17384c9b86847023c7d8aad03184b
= <diegogobbetti111@gmail.com>
2024-08-08 17:27:38 +0200

    realized cipherspec handling tls1.2

--- clientHello.py
+++ clientHello.py
@@ -12,9 +12,18 @@ status :  handled tls1.3 and tls1.2
 """"
 todo : 
     - handler of responses different than serverHello: changecipherspec, ... 
-        - I get chagnecipherspce beacuase if the server decide to use exchange method with psk ecdh, it expects a keyshare and I can't create a proper clienthello packet
-        - new branch and test
-            - potentially found the problem. lenght parameter of whole tls packet: trying to create more keys in raw format to provide more option to servers, shouldn't get changecipherspce then
+        - I can get chagnecipherspce for several reasons :
+            - if the server decide to use exchange method with psk ecdh, it expects a keyshare and I can't create a proper clienthello packet --> fixed
+            - server reveal that there is a previous session so it provieds a sessione id or ticket in the reply and skip verification.
+            - PSK-based handshake(TLS1.3), a changecipherspec is used and included in the server hello, to diguise the session as tls1.2 
+                for middleboxes compatibility(sw/hw in the middle of a connection that filter/modify packets)
+        the bottom line is in tls1.3 everythin that follows serverhello is encrypted with teh session key and authneticated, so are the certificates and certifcate verify messages. That appear as
+        application data in the following tls messsages --> watch test in scapy repo with tlssession mirror to decrypt
+        So I should decrypted the followign message to retrive certificate in tls1.3. Otherwise in tls1.2(wordreference.com) the certificate is sent in 
+        the serverhello message, which is not encypted.
+        So first of all I need to undestand the tls version sued by the server and accordingly proceed on the analysis. Anyway if I send a client hello with version tls1.2
+        (supported_ch_versions = [771]) I can get a certificate in the serverhello message, which is not encrypted.
+
     - prn or lower method should return a tls package crafted with the responses    
 """
 
@@ -36,18 +45,17 @@ import scapy.supersocket
     
 CIPHERS = [4866,4867,4865,49196,49200,159,52393,52392,52394,49195,49199,158,49188,49192,107,49187,49191,103,49162,49172,57,49161,49171,51,157,156,61,60,53,47,255]
 SUPP_CV = [24, 23, 22, 21, 29]
-SUPP_CV_test = 29
+SUPP_CV_test = [29]
 SIGN_ALGS = [1027,1283,1539,2055,2056,2057,2058,2059,2052,2053,2054,1025,1281,1537]
 
 sock = SuperSocket(family=socket.AF_INET,type=socket.SOCK_STREAM) 
 srv_rs = []
 
 def main():
-        dst = ["w3schools.com"]
-        ch_pk = clientHello(hostname=dst)
+        ch_pk = clientHello(hostname="www.target.com")
 
         sock.ins.bind(("192.168.1.46", 5899))
-        dstip=socket.gethostbyname("www.w3schools.com")
+        dstip=socket.gethostbyname("www.target.com")
         sock.ins.connect((dstip, 443))      
         print(dstip)
         # sock = socket.create_connection((dstip, 443))
@@ -153,14 +161,26 @@ def generateKeys(groups):
             
             
 
-def clientHello(version=772, ciphers=CIPHERS, groups=SUPP_CV, sign_algs=SIGN_ALGS, pubkeys=None, servername=None, pskkxmodes=0, hostname=None):
+def clientHello(version=772, ciphers=CIPHERS, groups=SUPP_CV_test, sign_algs=SIGN_ALGS, pubkeys=None, pskkxmodes=1, hostname=None):
         try:
-            ch_pk = TLS(version=769, type=22)/\
-                    TLSClientHello(version=771, ciphers=ciphers, random_bytes=os.urandom(32) , ext=[ \
-                                    TLS_Ext_SupportedGroups(groups=groups), TLS_Ext_SignatureAlgorithms(sig_algs=sign_algs), \
-                                    TLS_Ext_SupportedVersion_CH(versions=[version]), TLS_Ext_PSKKeyExchangeModes(kxmodes=[pskkxmodes]), \
-                                     TLS_Ext_KeyShare_CH(client_shares=[])
-                                    ]) # TLS_Ext_ServerName(servernames=hostname)
+            ch_pk = TLS(version=769, type=22, msg=[TLSClientHello(version=771, ciphers=ciphers, random_bytes=os.urandom(32) , ext=[ \
+                                    TLS_Ext_ServerName(servernames=[ServerName(nametype=0, servername=hostname.encode('utf-8'))]), TLS_Ext_SupportedGroups(groups=groups), \
+                                    TLS_Ext_SignatureAlgorithms(sig_algs=sign_algs), TLS_Ext_SupportedVersion_CH(versions=[771]), \
+                                    TLS_Ext_PSKKeyExchangeModes(kxmodes=[pskkxmodes]), TLS_Ext_SupportedPointFormat(ecpl=[0], type=11, len=2, ecpllen=1), \
+                                    TLS_Ext_KeyShare_CH(client_shares=[])])]) #TLS_Ext_ServerName(servernames=hostname),
+            """
+            version 1.2
+            """
+            ch_pk12 = TLS(version=769, type=22, msg=[TLSClientHello(version=771, msgtype=1,  ciphers=ciphers, random_bytes=os.urandom(32), comp=[0], ext=[ \
+                                    TLS_Ext_ServerName(servernames=[ServerName(nametype=0, servername=hostname.encode('utf-8'))], type=0), \
+                                    TLS_Ext_RenegotiationInfo(type=65281, len=1, reneg_conn_len=0, renegotiated_connection=b''), \
+                                    TLS_Ext_SupportedGroups(groups=groups), TLS_Ext_SignatureAlgorithms(sig_algs=sign_algs) \
+                                    # TLS_Ext_SupportedVersion_CH(versions=[version, 771]), TLS_Ext_PSKKeyExchangeModes(kxmodes=[pskkxmodes]), \
+                                    # TLS_Ext_KeyShare_CH(client_shares=[])
+                                    ])]) #
+            # MISSING from scapy repo example tls12 : gmt_unix_time, alcuni len parameters : namelen , servernameslen ....  , TLS_Ext_SessionTicket, TLS_Ext_NPN, TLS_Ext_ALPN, TLS_Ext_CSR
+
+
         except Exception as e:
             logging.log(40, "Error during client hello packet creation \n") 
             print(e)
@@ -174,12 +194,20 @@ def clientHello(version=772, ciphers=CIPHERS, groups=SUPP_CV, sign_algs=SIGN_ALG
         
         for curve, pu_key in zip(list(groups), pubkeys):
             print(pu_key)
-            ch_pk[TLSClientHello].ext[4].client_shares.append(KeyShareEntry(group=curve, key_exchange=pu_key, kxlen=len((pu_key))))
+            ch_pk[TLSClientHello].ext[6].client_shares.append(KeyShareEntry(group=curve, key_exchange=pu_key, kxlen=len((pu_key))))
 
 
-        print(ch_pk['TLS'].show())
+        # print(ch_pk['TLS'].show())
 
         ch_pk[TLS].len = len(raw(ch_pk[TLSClientHello]))
+        ch_pk12[TLS].len = len(raw(ch_pk12[TLS]))
+        # ch_pk12[TLS].msg[0].msglen = len(raw([ch_pk12[TLS].msg[0]]))
+        ext_len = 0
+        for e in ch_pk12[TLSClientHello].ext:
+            ext_len += len(raw(e))
+        ch_pk12[TLS].msg[0].extlen = ext_len
+
+        ch_pk.show()
 
         return ch_pk
 
